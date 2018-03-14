932494c0b0b842150a1c1c4f29c5aa0876e5e49f
VivekSingh <viveksi@vfsglobal.com>
2018-03-14 14:07:24 +0530

    compression added

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var compression = require('compression')
 //Install express server
 const express = require('express');
 const app = express();
-
+app.use(compression())
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.use(compression())
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
